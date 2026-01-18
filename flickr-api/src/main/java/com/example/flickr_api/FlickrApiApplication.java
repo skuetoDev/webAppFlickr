13089c6587a3a7aa4aa9287640b2bd73fe0d5826
skuetoDev <mariogn1987@gmail.com>
2026-01-18 18:14:56 +0100

    adding spring framework

---
+++ flickr-api/src/main/java/com/example/flickr_api/FlickrApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.flickr_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrApiApplication.class, args);
+	}
+
+}
---
+++ flickr-api/src/test/java/com/example/flickr_api/FlickrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flickr_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
