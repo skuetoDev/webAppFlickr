6a19aa50f452fa95e9e106facd15204e2013d0a5
skuetoDev <mariogn1987@gmail.com>
2026-01-18 18:41:23 +0100

    generate api configuration and model FlickrPhoto

---
+++ flickr-api/src/main/java/com/example/flickr_api/models/FlickrPhoto.java
@@ -0,0 +1,61 @@
+package com.example.flickr_api.models;
+
+public class FlickrPhoto {
+    private String id;
+    private String title;
+    private String thumbnailUrl;
+    private String imageUrl;
+    private String author;
+
+    // Constructor empty
+    public FlickrPhoto() {}
+
+    // Constructor with params
+    public FlickrPhoto(String id, String title, String thumbnailUrl, String imageUrl, String author) {
+        this.id = id;
+        this.title = title;
+        this.thumbnailUrl = thumbnailUrl;
+        this.imageUrl = imageUrl;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
