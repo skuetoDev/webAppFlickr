d448f08a90ad40fc97f60d46f921bfdffc953215
skuetoDev <mariogn1987@gmail.com>
2026-01-20 19:55:02 +0100

    creat photo-grid

--- flickr-Front/src/app/app.ts
+++ flickr-Front/src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Button } from './components/button/button';
 import { Header } from './components/header/header';
 import { SearchBar } from './components/search-bar/search-bar';
+import { PhotoCard } from "./components/photo-card/photo-card";
+import { PhotoGrid } from "./components/photo-grid/photo-grid";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, PhotoCard, PhotoGrid],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- flickr-Front/src/app/components/photo-grid/photo-grid.ts
+++ flickr-Front/src/app/components/photo-grid/photo-grid.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, output } from '@angular/core';
+import { PhotoCard } from "../photo-card/photo-card";
+import { FlickrPhoto } from '../../models/Photo';
 
 @Component({
   selector: 'app-photo-grid',
-  imports: [],
+  imports: [PhotoCard],
   templateUrl: './photo-grid.html',
   styleUrl: './photo-grid.css',
 })
 export class PhotoGrid {
+  photos = input.required<FlickrPhoto[]>();
+  loading = input<boolean>(false);
+  hasSearched = input<boolean>(false);
+  hasMore = input<boolean>(true);
 
+  onOpenPhoto = output<FlickrPhoto>();
 }
